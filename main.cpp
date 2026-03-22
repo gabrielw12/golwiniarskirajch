d7a3b3a3a00460ef5878170d0e49c72766d4e830
Gabriel <gabrielwiniarski11@gmail.com>
2026-03-22 18:43:21 +0100

    ostatni push

--- main.cpp
+++ main.cpp
@@ -5,7 +5,36 @@ using namespace std;
 
 class Board
 {
-  vector<int> height
+  size_t height =0;
+  size_t width =0;
+  vector <size_t> cmap;
+  vector <size_t> nmap;
+
+  public:
+
+  Board(size_t a, size_t b): height(a), width(b), cmap(a*b) {};
+  void count_neighbours(size_t & cell)
+  {
+    size_t counter =0;
+    for(size_t i = -1; i<2; i++)
+    {
+    if (cell - 1 + i*width)
+        ++counter;
+    if (cell + 1 + i*width)
+        ++counter;
+    }
+
+  }
+  void count_nmap(vector <size_t>cmap, vector <size_t> nmap)// tu chcialem obliczyc nowa mape na podstawie starej, ale zorientowalem sie
+  {
+    for (int i = 0; i< height*width; i++)
+    {
+        if (cmap[i])
+        {
+
+        }
+    }
+  }
   
 };
 
@@ -17,5 +46,6 @@ int main()
 
 
 /*
-zastanawiamy
+zastanawiamy się czy to powinna być tablica w postaci wektora wketrorów, czy pojedyńczy wektor
+
 */
